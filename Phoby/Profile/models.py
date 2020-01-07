a368ac17fad04f6c504675154fc81f5acf885c57
rujan <rujan209@gmail.com>
2020-01-07 20:08:26 +0545

    Added some models
    needs changes ( a lot )

--- Phoby/Profile/models.py
+++ Phoby/Profile/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    profile_ID = models.AutoField(primary_key=True)
+
+class User(models.Model):
+    user_ID = models.AutoField(primary_key=True)
+    user_uname = models.CharField(max_length = 100)
+    user_fname = models.CharField(max_length = 50)
+    user_lname = models.CharField(max_length = 50)
+    user_password = models.CharField(max_length = 30)
+
+class SavedPost(models.Model):
+    # post_ID = models.ForeignKey(Post.post_ID,on_delete=models.CASCADE)
+    user_ID = models.ForeignKey(User,on_delete=models.CASCADE)
+
--- Phoby/Profile/views.py
+++ Phoby/Profile/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from .models import Profile
 # Create your views here.
+def profile(request):
+    return render(request=request, template_name="Profile/profile.html", context={"Profile":Profile.objects.all})
