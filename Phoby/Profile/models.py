4f10f21b6f35cd8413e528ab9d87b81479e20bf2
rujan <rujan209@gmail.com>
2020-01-08 09:48:45 +0545

    edited urls,models,profile.html

--- Phoby/Profile/models.py
+++ Phoby/Profile/models.py
@@ -4,14 +4,5 @@ from django.db import models
 class Profile(models.Model):
     profile_ID = models.AutoField(primary_key=True)
 
-class User(models.Model):
-    user_ID = models.AutoField(primary_key=True)
-    user_uname = models.CharField(max_length = 100)
-    user_fname = models.CharField(max_length = 50)
-    user_lname = models.CharField(max_length = 50)
-    user_password = models.CharField(max_length = 30)
 
-class SavedPost(models.Model):
-    # post_ID = models.ForeignKey(Post.post_ID,on_delete=models.CASCADE)
-    user_ID = models.ForeignKey(User,on_delete=models.CASCADE)
 
