411a6cbcd2a3a22fe30147aa766c55fccc4d9c3f
rujan <rujan209@gmail.com>
2020-01-09 21:39:06 +0545

    added login
    login suprisingly worked (for now). test page opens.

--- Phoby/users/urls.py
+++ Phoby/users/urls.py
@@ -20,6 +20,7 @@ from . import views
 
 urlpatterns = [
     path('',views.homepage,name='homepage'),
-    path('',views.login,name='login'),
+    path('login',views.login,name='login'),
+    path('test',views.test,name='test'),
 ]
 
--- Phoby/users/views.py
+++ Phoby/users/views.py
@@ -1,26 +1,38 @@
 from django.shortcuts import render, redirect
 from .models import Users
 from django.contrib.auth.models import User, auth
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 # def homepage(request):
 #     return render(request, template_name="home.html", context={"users":Users.objects.all})
 
-
+def test(request):
+    return render(request, template_name="users/test.html")
+    
 def login(request):
     if request.method == 'POST':
-        username= request.POST['username']
-        password= request.POST['password']
-
-        user=auth.authenticate(username=username, password=password)
-
-        if user is not None:
-            auth.login(request,user)
-            return redirect('/test.html')
+        username = request.POST.get('username')
+        password =  request.POST.get('password')
+        # post = User.objects.filter(username=username)
+        post = authenticate(username=username,password=password)
+        if post is not None:
+            username = request.POST['username']
+            request.session['username'] = username
+            return redirect("test")
         else:
-            messages.info(request,'invalid')
-            return redirect('login')
+            return render(request,'users/login.html')
+    return render(request, template_name="users/login.html")
+
+        # user=authenticate(request, username=username, password=password)
+
+        # if user is not None:
+        #     login(request,user)
+        #     return render(request, template_name="home.html")
+        # else:
+        #     messages.info(request,'invalid')
+        #     return redirect('login')
+# return render(request, template_name="users/login.html")
 
 def homepage(request):
 
@@ -75,7 +87,7 @@ def homepage(request):
 
         return redirect('/')
     else:
-        return render(request, template_name="home.html", context={"users":Users.objects.all})
+        return render(request, template_name="users/home.html", context={"users":Users.objects.all})
 
 
 
