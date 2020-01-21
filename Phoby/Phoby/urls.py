5bb7167a3a92b1a08c8a46660ddb6518567abe97
Shahi33 <58820973+Shahi33@users.noreply.github.com>
2020-01-21 18:50:55 +0545

    login, logout
    register, login and logout completed

--- Phoby/Phoby/urls.py
+++ Phoby/Phoby/urls.py
@@ -1,4 +1,4 @@
-"""Phoby URL Configuration
+"""mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -16,8 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-
 urlpatterns = [
-    path('',include('users.urls')),
-    path('admin/',admin.site.urls),
+    path('', include('accounts.urls')),
+    path('admin/', admin.site.urls),
 ]
