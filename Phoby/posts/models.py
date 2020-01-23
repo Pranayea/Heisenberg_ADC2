ca5109afcc035323fa9cd0b020daede25e5dc455
Pranayea <58832325+Pranayea@users.noreply.github.com>
2020-01-23 16:32:02 +0545

    Commit try2 Post

---
+++ Phoby/posts/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from datetime import datetime, date
+# Create your models here.
+
+class createPosts(models.Model):
+    post_id = models.AutoField(primary_key = True)
+    # post_image = models.ImageField(upload_to='posts_images/',blank=False)
+    post_caption = models.TextField(max_length=140)
+    uploaded_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.post_caption
+        
--- Phoby/posts/urls.py
+++ Phoby/posts/urls.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from django.contrib import admin
 from django.urls import path
 from . import views
@@ -10,3 +11,14 @@ urlpatterns = [
     path('upload/<int:pk>/', views.update_posts, name='update'),
     path('view/<int:pk>/',views.delete_posts,name='delete'),
 ]
+=======
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('posts/',views.posts,name='posts'),
+    path('view/',views.posts_view,name='view'),
+    path('<int:pk>/',views.posts_edit,name='posts_update'),
+    path('delete/<int:pk>/',views.posts_delete,name='posts_delete'),
+    ]
+>>>>>>> parent of be3fd2b... 1stpost delete
