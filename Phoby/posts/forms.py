3adc5f3186980df4b570d15076e72c3f2e0b16b9
Shahi33 <58820973+Shahi33@users.noreply.github.com>
2020-01-23 17:11:48 +0545

    test delete
    delete posts fro testing

--- Phoby/posts/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import createPosts
-
-class OurForm(forms.ModelForm):
-# Meta Class Is used to override a class
-    class Meta:
-        model = createPosts
-        fields = ('post_image','post_caption')
