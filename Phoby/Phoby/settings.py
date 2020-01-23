67906d5309c1aa5a4c31c7ecb2b5fb164a53f490
rujan <rujan209@gmail.com>
2020-01-23 16:53:08 +0545

    starting fresh part 2
    because the first time didn't work

--- Phoby/Phoby/settings.py
+++ Phoby/Phoby/settings.py
@@ -37,9 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'Profile',
-    'users.apps.UsersConfig',
-    'posts.apps.PostsConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -121,6 +119,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT =  os.path.join(BASE_DIR, 'posts/media')
