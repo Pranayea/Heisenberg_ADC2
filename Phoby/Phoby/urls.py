6241b645d6b28278231987f93e04d353e7644641
Pranayea <58832325+Pranayea@users.noreply.github.com>
2020-01-24 10:54:57 +0545

    Comiit ni jhyau lagisakyo

--- Phoby/Phoby/urls.py
+++ Phoby/Phoby/urls.py
@@ -19,10 +19,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-   path('', include('accounts.urls')),
+   path('accounts/', include('accounts.urls')),
     path('admin/',admin.site.urls),
     path('posts/', include('posts.urls')),
-    path('', include('search.urls')),
+    path('search/', include('search.urls')),
 
 ]
 urlpatterns += static(settings.MEDIA_URL ,document_root = settings.MEDIA_ROOT)
