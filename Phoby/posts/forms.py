6019a1ef9d3e7f135ad02af4d5297abb3c868daa
rujan <rujan209@gmail.com>
2020-02-10 17:20:46 +0545

    cleanup 2
    ..?

--- Phoby/posts/forms.py
+++ Phoby/posts/forms.py
@@ -6,9 +6,10 @@ class OurForm(forms.ModelForm):
     # Meta Class Is used to override a class
     class Meta:
         model = CreatePosts
-        fields = ('post_image','post_caption','uploaded_by')
+        fields = ('post_image', 'post_caption', 'uploaded_by')
+
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comments
-        fields=('comment',)
+        fields = ('comment',)
